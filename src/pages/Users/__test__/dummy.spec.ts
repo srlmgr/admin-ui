ed1f003374bd4fe74bbdeed2fc26da9e7c537038
mpapenbr <Markus.Papenbrock+github@gmail.com>
2026-05-15 08:59:04 +0200

    updated base config

---
+++ src/pages/Users/__test__/dummy.spec.ts
@@ -0,0 +1,5 @@
+describe("Dummy Test", () => {
+	it("should pass a dummy test", () => {
+		expect(true).toBe(true);
+	});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
-  },
-})
+	test: {
+		globals: true,
+	},
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+			},
+		},
+	},
+});
